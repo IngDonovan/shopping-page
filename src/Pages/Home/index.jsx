2320dc3bfa46af36ddd0988e2104490eab21c69b
Donovan Rojas <rojasdrm03@gmail.com>
2023-06-26 15:58:49 -0500

    restAPI base

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,16 +1,27 @@
+import { useState, useEffect } from "react";
 import Card from "../../Components/Card";
 import Layout from "../../Components/layout";
 
 function Home() {
-
+  const [items, setItems] = useState(null);
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products?')
+      .then(response => response.json())
+      .then(data => setItems(data))
+  }, [])
     return (
-      <>
-        <Layout>
+       <Layout>
           Home
-          <Card />
-        </Layout>
+          {
+            items?.map((item) => 
+              (<Card key={item.id} data={item} />
+              )
+            )
+          }
         
-      </>
+      </Layout>
+        
+
     )
   }
   
