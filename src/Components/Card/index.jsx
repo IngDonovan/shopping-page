b4a083a856955e30576bb417f99fbd9f7b05c3ee
Donovan Rojas <rojasdrm03@gmail.com>
2023-06-29 10:29:12 -0500

    Context in shopping Car

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,11 +1,19 @@
+import { useContext } from "react";
+import { ShoppingCartContext } from "../../Context/intex";
+
 const Card = ({data}) => {
+    const context = useContext(ShoppingCartContext);
     return (
         <article className="bg-[#454545] cursor-pointer w-56 h-60 rounded-lg shadow-[15px_15px_30px_rgba(25,25,25),-15px_-15px_30px_rgba(60,60,60)]">
             <figure className="relative mb-2 w-full h-4/5">
                 <span className="absolute bottom-0 left-0 rounded-lg bg-[#212121]/60 text-xs m-2 px-3 py-0.5">{data.category}</span>
                 <img className="w-full h-full object-cover  rounded-lg"
                 src={data.image} alt="headphones" />
-                <button type="button" className="absolute top-0 right-0 m-2 flex justify-center items-center bg-[#454545] w-7 h-7 rounded-full text-white text-xl shadow-[15px_15px_30px_rgba(25,25,25),-15px_-15px_30px_rgba(60,60,60)]">+</button>
+                <button type="button" className="absolute top-0 right-0 m-2 flex justify-center items-center bg-[#454545] w-7 h-7 rounded-full text-white text-xl shadow-[15px_15px_30px_rgba(25,25,25),-15px_-15px_30px_rgba(60,60,60)]"
+                onClick={() => context.setCount(context.count + 1)}
+                >
+                    +
+                </button>
             </figure>
             <p className="flex justify-between">
                 <span className="text-sm font-light mx-2 ">{data.title}</span>
--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,83 +1,15 @@
+import { useContext } from "react";
 import { NavLink } from "react-router-dom";
+import { ShoppingCartContext } from "../../Context/intex";
 
 const Navbar = () => {
+    const context = useContext(ShoppingCartContext);
+
     const activeStyle = 'underline underline-offset-4';
     const classlink = ({isActive}) => isActive ? activeStyle : undefined;
 
 
     return(
-        // <nav className="flex justify-between items-center z-10 w-full py-5 px-8 text-sm font-light text-white bg-black">
-        //     <ul className="flex items-center gap-3">
-        //         <li className="font-bold text-lg">
-        //             <NavLink to='/'>
-        //                 ShopING
-        //             </NavLink>
-        //         </li>
-        //         <li>
-        //             <NavLink to='/'
-        //                 className = {classlink}>
-        //                 All
-        //             </NavLink>
-        //         </li>
-        //         <li>
-        //             <NavLink to='/clothes'
-        //                 className = {classlink}>
-        //                 Clothes
-        //             </NavLink>
-        //         </li>
-        //         <li>
-        //             <NavLink to='/electronics'
-        //                 className = {classlink}>
-        //                 Electronics
-        //             </NavLink>
-        //         </li>
-        //         <li>
-        //             <NavLink to='/furnitures'
-        //                 className = {classlink}>
-        //                 Furnitures
-        //             </NavLink>
-        //         </li>
-        //         <li>
-        //             <NavLink to='/toys'
-        //                 className = {classlink}>
-        //                 Toys
-        //             </NavLink>
-        //         </li>
-        //         <li>
-        //             <NavLink to='/others'
-        //                 className = {classlink}>
-        //                 Others
-        //             </NavLink>
-        //         </li>
-        //     </ul>
-        //     <ul className="flex items-center gap-3">
-        //         <li className="text-white/40">
-        //             email@gmail.com
-        //         </li>
-        //         <li>
-        //             <NavLink to='/my-orders'
-        //             className = {classlink}>
-        //                 My Orders
-        //             </NavLink>
-        //         </li>
-        //         <li>
-        //             <NavLink to='/my-account'
-        //             className = {classlink}>
-        //                 My Account
-        //             </NavLink>
-        //         </li>
-        //         <li>
-        //             <NavLink to='/sign-in'
-        //             className = {classlink}>
-        //                 Sign In
-        //             </NavLink>
-        //         </li>
-        //         <li>
-        //             🛒 0 
-        //         </li>
-                
-        //     </ul>
-        // </nav>
         <nav className="flex justify-between items-center z-10 w-full py-5 px-8 text-sm font-light text-white bg-black">
             <div className="flex flex-wrap items-center gap-3">
                 <NavLink to='/' className="font-bold text-lg">
@@ -139,7 +71,7 @@ const Navbar = () => {
                         Sign In
                         </NavLink>
                     </li>
-                    <li>🛒 0</li>
+                    <li>🛒 {context.count}</li>
                 </ul>
             </div>
             </nav>
