3d289f0c1ed968a1424ec6b80f01a4193a5d79a3
Donovan Rojas <rojasdrm03@gmail.com>
2023-06-29 10:36:27 -0500

    Context

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -2,7 +2,7 @@ import { useContext } from "react";
 import { ShoppingCartContext } from "../../Context/intex";
 
 const Card = ({data}) => {
-    const context = useContext(ShoppingCartContext);
+    const { count, setCount} = useContext(ShoppingCartContext);
     return (
         <article className="bg-[#454545] cursor-pointer w-56 h-60 rounded-lg shadow-[15px_15px_30px_rgba(25,25,25),-15px_-15px_30px_rgba(60,60,60)]">
             <figure className="relative mb-2 w-full h-4/5">
@@ -10,7 +10,7 @@ const Card = ({data}) => {
                 <img className="w-full h-full object-cover  rounded-lg"
                 src={data.image} alt="headphones" />
                 <button type="button" className="absolute top-0 right-0 m-2 flex justify-center items-center bg-[#454545] w-7 h-7 rounded-full text-white text-xl shadow-[15px_15px_30px_rgba(25,25,25),-15px_-15px_30px_rgba(60,60,60)]"
-                onClick={() => context.setCount(context.count + 1)}
+                onClick={() => setCount(count + 1)}
                 >
                     +
                 </button>
