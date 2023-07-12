b0a8351fd1470eddd8953b6ca03962e36b304f4a
Donovan Rojas <rojasdrm03@gmail.com>
2023-07-12 11:33:14 -0500

    styles OrdersCard

--- src/Components/OrdersCard/index.jsx
+++ src/Components/OrdersCard/index.jsx
@@ -22,16 +22,16 @@ const OrdersCard = props => {
             <div className="flex justify-between items-center mb-3 border rounded-lg p-3">
                 <div className="flex items-center justify-between grow gap-2 px-4">
                     <div className="flex gap-1 items-center justify-center">
+                        <CalendarDaysIcon className="h-6 w-6 text-white" />
+                        <p className="font-normal   text-sm">{date}</p>
+                    </div>
+                    <div className="flex gap-1 items-center justify-center">
                         <ShoppingCartIcon className="h-6 w-6 text-white" />
                         <p className="font-normal  text-sm">{`${totalProducts} ${totalProducts === 1 ? "producto" : "productos"}`}</p>
                     </div>
                     <div className="flex gap-1 items-center justify-center">
                         <CurrencyDollarIcon className="h-6 w-6 text-white" />
-                        <p className="font-normal text-sm">${totalPrice}</p>
-                    </div>
-                    <div className="flex gap-1 items-center justify-center">
-                        <CalendarDaysIcon className="h-6 w-6 text-white" />
-                        <p className="font-normal   text-sm">{date}</p>
+                        <p className="font-normal text-base ">${totalPrice}</p>
                     </div>
                 </div>
                 <div className="flex items-center gap-2">
