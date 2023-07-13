72d21cb2378a66861df6cc9fae6996b024cdf4b1
Donovan Rojas <rojasdrm03@gmail.com>
2023-07-13 09:58:17 -0500

    quickly styles to signin and Account

--- src/Pages/MyAccount/index.jsx
+++ src/Pages/MyAccount/index.jsx
@@ -1,11 +1,20 @@
 import Layout from "../../Components/layout";
+import './styles.css'
 
 function MyAccount() {
 
     return (
       <>
         <Layout>
-          MyAccount
+        <div class="loader my-10">
+          <div class="wrapper">
+            <div class="circle"></div>
+            <div class="line-1"></div>
+            <div class="line-2"></div>
+            <div class="line-3"></div>
+            <div class="line-4"></div>
+          </div>
+        </div>
         </Layout>
       </>
     )
--- src/Pages/SignIn/index.jsx
+++ src/Pages/SignIn/index.jsx
@@ -1,11 +1,27 @@
 import Layout from "../../Components/layout";
+import './styles.css';
 
 function SignIn() {
 
     return (
       <>
         <Layout>
-          SignIn
+        <div className="login-box">
+          <form>
+            <div className="user-box">
+              <input type="text" name="" required="" />
+              <label>Username</label>
+            </div>
+            <div class="user-box">
+              <input type="password" name="" required="" />
+              <label>Password</label>
+            </div><center>
+            <a href="#">
+                    Login
+                <span></span>
+            </a></center>
+          </form>
+          </div>
         </Layout>
       </>
     )
